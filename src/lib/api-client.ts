204e65044646cd7b6af1bc1588e65e10567fb326
mcs <maicongson69@gmail.com>
2024-08-16 09:55:03 +0700

    fix: navigate to login route if authentication failed

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:8080",
@@ -7,9 +7,9 @@ export const api = axios.create({
 
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
+  (error: AxiosError) => {
     // Navigate to the login route when authentication failed
-    if (error.response.headers.unauthorized) {
+    if (error.response?.status === 401) {
       window.location.href = "/auth";
     }
     return Promise.reject(error);
